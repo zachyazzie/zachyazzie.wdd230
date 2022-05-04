3ab2d30075998daa46693c4bd5fb8380be59c88e
zachyazzie <zacharyyazzie94@gmail.com>
2022-05-03 23:53:29 -0600

    Not sold on these placements yet.

--- weather/js/index.js
+++ weather/js/index.js
@@ -6,30 +6,37 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-// Date in footer
+// Date in footer & Display message on Friday
+
 const d = new Date()
 var dayNumber = d.getDay()
 let dayOfWeek
 if (dayNumber === 0) {
     dayOfWeek = 'Sunday';
+    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 1) {
     dayOfWeek = 'Monday';
+    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 2) {
     dayOfWeek = 'Tuesday';
+    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 3) {
     dayOfWeek = 'Wednesday';
+    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 4) {
     dayOfWeek = 'Thursday';
+    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 5) {
     dayOfWeek = 'Friday';
 }
 else {
     dayOfWeek = 'Saturday';
+    document.getElementById('friday-banner').style.display='none'
 }
 
 let dayInMonth = d.getDate();
@@ -38,3 +45,5 @@ let month = d.toLocaleString('default', { month: 'long' });
 let fullDate = `${dayOfWeek}, ${dayInMonth} ${month} ${year}`;
 
 document.getElementById("date-in-footer").innerHTML = fullDate;
+
+
