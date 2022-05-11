67efd5482eae94405b87789e7fa0f9141b3fa308
zachyazzie <zacharyyazzie94@gmail.com>
2022-05-10 18:00:03 -0600

    working on gallery page

--- lesson-4/index.js
+++ lesson-4/index.js
@@ -1,7 +1,23 @@
-// placeholder images not changing.
-// Look at MDM documentation
+// /** First we get all the non-loaded image elements **/
+// var lazyImages = [].slice.call(document.querySelectorAll(".lazy-loaded-image.lazy"));
+// /** Then we set up a intersection observer watching over those images and whenever any of those becomes visible on the view then replace the placeholder image with actual one, remove the non-loaded class and then unobserve for that element **/
+// let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+//     entries.forEach(function(entry) {
+//         if (entry.isIntersecting) {
+//             let lazyImage = entry.target;
+//             lazyImage.src = lazyImage.dataset.src;
+//             lazyImage.classList.remove("lazy");
+//             lazyImageObserver.unobserve(lazyImage);
+//         }
+//     });
+// });
+// /** Now observe all the non-loaded images using the observer we have setup above **/
+// lazyImages.forEach(function(lazyImage) {
+//     lazyImageObserver.observe(lazyImage);
+// });
 
-const images = document.querySelectorAll('[data-src]');
+const images = document.querySelectorAll("[data-src]");
+let imgBlur = document.querySelectorAll('img');
 
 function preloadImage(img) {
     const src = img.getAttribute("data-src");
@@ -24,6 +40,7 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
     })
 }, imgOptions);
 
-imagesToLoad.forEach(image => {
+images.forEach(image => {
     imgObserver.observe(image);
-})
+});
+
---
+++ weather/js/lazyload.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll("[data-src]");
+let imgBlur = document.querySelectorAll('img');
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const imgOptions = {};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+
