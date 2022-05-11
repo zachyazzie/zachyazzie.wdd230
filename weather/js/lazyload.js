bac4b1bdee917b76827fe3446efd8057fadf67d1
zachyazzie <zacharyyazzie94@gmail.com>
2022-05-11 15:53:51 -0600

    fixed lazy loading

--- weather/js/lazyload.js
+++ weather/js/lazyload.js
@@ -1,28 +1,25 @@
-const images = document.querySelectorAll("[data-src]");
-let imgBlur = document.querySelectorAll('img');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
-    img.src = src;
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
-
-const imgOptions = {};
-
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
-
-images.forEach(image => {
-    imgObserver.observe(image);
-});
-
