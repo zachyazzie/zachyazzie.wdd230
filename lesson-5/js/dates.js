60fbf7ef57c8baa3f82743b987a97f0eb1277b06
zachyazzie <zacharyyazzie94@gmail.com>
2022-05-17 16:02:57 -0600

    working on aligning images

--- lesson-5/js/dates.js
+++ lesson-5/js/dates.js
@@ -8,30 +8,24 @@ var dayNumber = d.getDay();
 let dayOfWeek;
 if (dayNumber === 0) {
     dayOfWeek = 'Sunday';
-    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 1) {
     dayOfWeek = 'Monday';
-    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 2) {
     dayOfWeek = 'Tuesday';
-    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 3) {
     dayOfWeek = 'Wednesday';
-    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 4) {
     dayOfWeek = 'Thursday';
-    document.getElementById('friday-banner').style.display='none'
 }
 else if (dayNumber === 5) {
     dayOfWeek = 'Friday';
 }
 else {
     dayOfWeek = 'Saturday';
-    document.getElementById('friday-banner').style.display='none'
 }
 
 let dayInMonth = d.getDate();
---
+++ lesson-5/js/townData.js
@@ -0,0 +1,54 @@
+let townList=[];
+
+const output = (listOfTowns) => {
+    console.log(listOfTowns);
+    listOfTowns.forEach(town => {
+        //Available elements in the list:
+        //name
+        //photo
+        //motto
+        //yearFounded
+        //currentPopulation
+        //averageRainfall
+        //events['...',...]
+
+        let article = document.createElement("article");
+        let townInfoDiv = document.createElement("div");
+        townInfoDiv.setAttribute('class', 'town-info');
+        let townImgDiv = document.createElement("div");
+        townImgDiv.setAttribute('class', 'town-img');
+        let townName = document.createElement("h2");
+        townName.innerText = town.name;
+        let imageElement = document.createElement("img");
+        imageElement.setAttribute('src', `assets/${town.photo}`);
+        imageElement.setAttribute('alt', town.name);
+        let motto = document.createElement("h3");
+        motto.innerText = town.motto;
+        motto.setAttribute('class', 'motto');
+        let yearFounded = document.createElement("p");
+        yearFounded.innerText = `Year Founded: ${town.yearFounded}`;
+        let currentPopulation = document.createElement("p");
+        currentPopulation.innerText = `Population: ${town.currentPopulation}`;
+        let averageRainfall = document.createElement("p");
+        averageRainfall.innerText = `Average Rain Fall: ${town.averageRainfall}`;
+
+        townInfoDiv.appendChild(townName);
+        townInfoDiv.appendChild(motto);
+        townInfoDiv.appendChild(yearFounded);
+        townInfoDiv.appendChild(currentPopulation);
+        townInfoDiv.appendChild(averageRainfall);
+        townImgDiv.appendChild(imageElement);
+        
+        article.appendChild(townInfoDiv);
+        article.appendChild(townImgDiv);
+        document.querySelector('#town-cards-home').appendChild(article);
+    })
+};
+
+fetch('https://byui-cit230.github.io/weather/data/towndata.json')
+    .then(response => response.json())
+    .then(towns => {
+        townList = towns.towns;
+        console.log(towns)
+        output(townList);
+    });
